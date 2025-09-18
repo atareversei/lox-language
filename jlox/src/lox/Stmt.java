0ef707f335e3e7e1c712566c93881b9eb1ddc297
atareversei (vivobook) <atareversei@gmail.com>
2025-09-18 22:42:41 +0330

    feat: add function declarations

--- jlox/src/lox/Stmt.java
+++ jlox/src/lox/Stmt.java
@@ -5,12 +5,20 @@ import java.util.List;
 abstract class Stmt {
   interface Visitor<R> {
     R visitBlockStmt(Block stmt);
+
     R visitExpressionStmt(Expression stmt);
+
+    R visitFunctionStmt(Function stmt);
+
     R visitIfStmt(If stmt);
+
     R visitPrintStmt(Print stmt);
+
     R visitVarStmt(Var stmt);
+
     R visitWhileStmt(While stmt);
   }
+
   static class Block extends Stmt {
     Block(List<Stmt> statements) {
       this.statements = statements;
@@ -35,6 +43,23 @@ abstract class Stmt {
     }
 
     final Expr expression;
+  }
+
+  static class Function extends Stmt {
+    Function(Token name, List<Token> params, List<Stmt> body) {
+      this.name = name;
+      this.params = params;
+      this.body = body;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitFunctionStmt(this);
+    }
+
+    final Token name;
+    final List<Token> params;
+    final List<Stmt> body;
   }
 
   static class If extends Stmt {
