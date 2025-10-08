26aaf73b1e9823764405e1919f7eea600a45b505
atareversei (vivobook) <atareversei@gmail.com>
2025-10-08 12:47:35 +0330

    feat: add constants

--- clox/main.c
+++ clox/main.c
@@ -5,6 +5,12 @@
 int main(int argc, char* argv[]) {
   Chunk chunk;
   initChunk(&chunk);
+
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT);
+  writeChunk(&chunk, constant);
+
+
   writeChunk(&chunk, OP_RETURN);
   writeChunk(&chunk, OP_RETURN);
 
