bbf8c4e36231fc3b431c97f55537e6e68faad0e8
atareversei (vivobook) <atareversei@gmail.com>
2025-10-15 18:05:00 +0330

    feat: add compiler numbers, grouping, and unary

--- clox/vm.c
+++ clox/vm.c
@@ -80,6 +80,18 @@ static InterpreterResult run() {
 }
 
 InterpreterResult interpret(const char* source) {
-  compile(source);
-  return INTERPRET_OK;
+  Chunk chunk;
+  initChunk(&chunk);
+
+  if(!compile(source, &chunk)) {
+    freeChunk(&chunk);
+    return INTERPRET_COMPILE_ERROR;
+  }
+
+  vm.chunk = &chunk;
+  vm.ip = vm.chunk->code;
+
+  InterpreterResult result = run();
+  freeChunk(&chunk);
+  return result;
 }
