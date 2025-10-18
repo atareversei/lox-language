fbda8aa892714cceb076866b1b4cc8ea5910dd12
atareversei (vivobook) <atareversei@gmail.com>
2025-10-18 23:09:17 +0330

    feat: add string concatenation + memory handling for objects

--- clox/object.h
+++ clox/object.h
@@ -17,15 +17,20 @@ typedef enum {
 
 struct Obj {
   ObjType type;
+  struct Obj* next;
 };
 
 struct ObjString {
   Obj obj;
   int length;
-  char* chars;
+  char *chars;
 };
 
-ObjString* copString(const char* chars, int length);
+ObjString *takeString(char *chars, int length);
+
+ObjString *copyString(const char *chars, int length);
+
+void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
   return IS_OBJ(value) && AS_OBJ(value)->type == type;
