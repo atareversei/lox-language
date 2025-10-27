358011234a3e84d6d9b0430eb7081dcc7054861f
atareversei (vivobook) <atareversei@gmail.com>
2025-10-27 16:55:06 +0330

    feat: add delete functionality to hash table

--- clox/table.c
+++ clox/table.c
@@ -21,9 +21,16 @@ void freeTable(Table *table) {
 
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
   uint32_t index = key->hash % capacity;
+  Entry *tombstone = NULL;
   for (;;) {
     Entry *entry = &entries[index];
-    if (entry->key == key || entry->key == NULL) {
+    if (entry->key == NULL) {
+      if (IS_NIL(entry->value)) {
+        return tombstone != NULL ? tombstone : entry;
+      } else {
+        if (tombstone == NULL) tombstone = entry;
+      }
+    } else if (entry->key == key) {
       return entry;
     }
 
@@ -31,20 +38,43 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
   }
 }
 
-static void adjustCapacity(Table* table, int capacity) {
-  Entry* entries = ALLOCATE(Entry, capacity);
+bool tableGet(Table *table, ObjString *key, Value *value) {
+  if (table->count == 0) return false;
+
+  Entry *entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL) return false;
+
+  *value = entry->value;
+  return true;
+}
+
+bool tableDelete(Table *table, ObjString *key) {
+  if (table->count == 0) return false;
+
+  Entry *entry = findEntry(table->entries, table->capacity, key);
+  if (entry->key == NULL) return false;
+
+  entry->key = NULL;
+  entry->value = BOOL_VAL(true);
+  return true;
+}
+
+static void adjustCapacity(Table *table, int capacity) {
+  Entry *entries = ALLOCATE(Entry, capacity);
   for (int i = 0; i < capacity; i++) {
     entries[i].key = NULL;
     entries[i].value = NIL_VAL;
   }
+  table->count = 0;
 
   for (int i = 0; i < table->capacity; i++) {
-    Entry* entry = &table->entries[i];
+    Entry *entry = &table->entries[i];
     if (entry->key == NULL) continue;
 
-    Entry* dest = findEntry(entries, capacity, entry->key);
+    Entry *dest = findEntry(entries, capacity, entry->key);
     dest->key = entry->key;
     dest->value = entry->value;
+    table->count++;
   }
 
   FREE_ARRAY(Entry, table->entries, table->capacity);
@@ -60,18 +90,34 @@ bool tableSet(Table *table, ObjString *key, Value value) {
 
   Entry *entry = findEntry(table->entries, table->capacity, key);
   bool isNewKey = entry->key == NULL;
-  if (isNewKey) table->count++;
+  if (isNewKey && IS_NIL(entry->value)) table->count++;
 
   entry->key = key;
   entry->value = value;
   return isNewKey;
 }
 
-void tableAddAll(Table* from, Table* to) {
+void tableAddAll(Table *from, Table *to) {
   for (int i = 0; i < from->capacity; i++) {
-    Entry* entry = &from->entries[i];
+    Entry *entry = &from->entries[i];
     if (entry->key != NULL) {
       tableSet(to, entry->key, entry->value);
     }
+  }
+}
+
+ObjString *tableFindString(Table *table, const char *chars, int length, uint32_t hash) {
+  if (table->count == 0) return NULL;
+
+  uint32_t index = hash % table->capacity;
+  for (;;) {
+    Entry *entry = &table->entries[index];
+    if (entry->key == NULL) {
+      if (IS_NIL(entry->value)) return NULL;
+    } else if (entry->key->length == length && entry->key->hash == hash && memcmp(entry->key->chars, chars, length) == 0) {
+      return entry->key;
+    }
+
+    index = (index + 1) % table->capacity;
   }
 }
--- clox/table.h
+++ clox/table.h
@@ -16,11 +16,11 @@ typedef struct {
 } Table;
 
 void initTable(Table *table);
-
 void freeTable(Table *table);
-
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableDelete(Table* table, ObjString* key);
 bool tableSet(Table *table, ObjString *key, Value value);
-
 void tableAddAll(Table* from, Table* to);
+ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash);
 
 #endif
