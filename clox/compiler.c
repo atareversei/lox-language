44eef788450df6b495b63cb4d9132ab91509a969
atareversei (vivobook) <atareversei@gmail.com>
2025-10-29 23:18:43 +0330

    feat: add if statement

--- clox/compiler.c
+++ clox/compiler.c
@@ -122,6 +122,13 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
   emitByte(byte2);
 }
 
+static int emitJump(uint8_t instruction) {
+  emitByte(instruction);
+  emitByte(0xff);
+  emitByte(0xff);
+  return currentChunk()->count - 2;
+}
+
 static void emitReturn() {
   emitByte(OP_RETURN);
 }
@@ -138,6 +145,17 @@ static uint8_t makeConstant(Value value) {
 
 static void emitConstant(Value value) {
   emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void patchJump(int offset) {
+  int jump = currentChunk()->count - offset - 2;
+
+  if (jump > UINT16_MAX) {
+    error("Too much code to jump over");
+  }
+
+  currentChunk()->code[offset] = (jump>>8) & 0xff;
+  currentChunk()->code[offset+1] = jump & 0xff;
 }
 
 static void initCompiler(Compiler *compiler) {
@@ -186,7 +204,7 @@ static void defineVariable(uint8_t global);
 
 static void declareVariable();
 
-static int resolveLocal(Compiler* compiler, Token* name);
+static int resolveLocal(Compiler *compiler, Token *name);
 
 static void binary(bool canAssign) {
   TokenType operatorType = parser.previous.type;
@@ -274,6 +292,23 @@ static void expressionStatement() {
   emitByte(OP_POP);
 }
 
+static void ifStatement() {
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition");
+
+  int thenJump = emitJump(OP_JUMP_IF_FALSE);
+  emitByte(OP_POP);
+  statement();
+
+  int elseJump = emitJump(OP_JUMP);
+
+  patchJump(thenJump);
+  emitByte(OP_POP);
+  if (match(TOKEN_ELSE)) statement();
+  patchJump(elseJump);
+}
+
 static void printStatement() {
   expression();
   consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -314,6 +349,8 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_IF)) {
+    ifStatement();
   } else if (match(TOKEN_LEFT_BRACE)) {
     beginScope();
     block();
@@ -475,9 +512,9 @@ static bool identifierEqual(Token *a, Token *b) {
   return memcmp(a->start, b->start, a->length);
 }
 
-static int resolveLocal(Compiler* compiler, Token* name) {
+static int resolveLocal(Compiler *compiler, Token *name) {
   for (int i = compiler->localCount - 1; i >= 0; i--) {
-    Local* local = &compiler->locals[i];
+    Local *local = &compiler->locals[i];
     if (identifierEqual(name, &local->name)) {
       if (local->depth == -1) {
         error("Can't read local variable in its own initializer.");
