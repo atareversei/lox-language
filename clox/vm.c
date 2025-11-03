40b0cdab9a0ffcdfa0387f9e7b6573d97db26b1f
atareversei (vivobook) <atareversei@gmail.com>
2025-11-03 21:38:35 +0330

    feat: add loops

--- clox/vm.c
+++ clox/vm.c
@@ -209,6 +209,11 @@ static InterpreterResult run() {
         if (isFalsy(peek(0))) vm.ip += offset;
         break;
       }
+      case OP_LOOP: {
+        uint16_t offset = READ_SHORT();
+        vm.ip -= offset;
+        break;
+      }
       case OP_RETURN:
         return INTERPRET_OK;
     }
