dc49e7740608eaefe81816051365acf1aa698da0
atareversei (vivobook) <atareversei@gmail.com>
2025-11-14 11:24:01 +0330

    feat: add methods

--- clox/memory.c
+++ clox/memory.c
@@ -75,9 +75,16 @@ static void blackenObject(Obj *object) {
 #endif
 
   switch (object->type) {
+    case OBJ_BOUND_METHOD: {
+      ObjBoundMethod *bound = (ObjBoundMethod*)object;
+      markValue(bound->receiver);
+      markObject((Obj*)bound->method);
+      break;
+    }
     case OBJ_CLASS: {
       ObjClass *klass = (ObjClass *) object;
       markObject((Obj *) klass->name);
+      markTable(&klass->methods);
       break;
     }
     case OBJ_CLOSURE: {
@@ -116,7 +123,12 @@ static void freeObject(Obj *object) {
 #endif
 
   switch (object->type) {
+    case OBJ_BOUND_METHOD: {
+      FREE(ObjBoundMethod, object);
+      break;
+    }
     case OBJ_CLASS: {
+      ObjClass *klass = (ObjClass*) object;
       FREE(ObjClass, object);
       break;
     }
